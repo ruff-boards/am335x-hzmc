2b191f837c4db7a7c4dc53d30d36689885b1cb79
Young <young.mu@aliyun.com>
2017-11-17 10:44:53 +0800

    Update src logic

--- template/src/index.js
+++ template/src/index.js
@@ -6,31 +6,7 @@ $.ready(function (error) {
         return;
     }
 
-    var fs = require('fs');
-    var RSON = require('rson');
-    var DPE = require('data-parsing-engine');
-    var DI = require('data-integration');
-    var DU = require('data-upload');
-
-    var appConfigFile = fs.readFileSync('configs/app-config.json');
-    var appConfigRson = RSON.rson(RSON.binify(JSON.parse(appConfigFile)));
-
-    var dpe = new DPE(appConfigRson, { debug: false });
-    dpe.start();
-    dpe.on('message', function (message) {
-        //console.log(JSON.stringify(message, false, 4));
-        di.store(message);
-    });
-
-    var di = new DI(appConfigRson, { debug: false });
-    di.start();
-    di.on('message', function (message) {
-        //console.log(JSON.stringify(message, false, 4));
-        du.store(message);
-    });
-
-    var du = new DU(appConfigRson, { debug: true });
-    du.start();
+    console.log('Hello, Ruff');
 });
 
 $.end(function () {
